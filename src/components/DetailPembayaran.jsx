1416ae924c06d617612b1ddef6669624effef7c1
nikodemushs <nikodemushs@gmail.com>
2024-06-05 19:01:33 +0700

    added responsive on booking detail pages and detail pembayaran component

--- src/components/DetailPembayaran.jsx
+++ src/components/DetailPembayaran.jsx
@@ -31,24 +31,26 @@ export default function DetailPembayaran() {
   };
 
   return (
-    <div className="w-[535px]">
-      <div className="pb-4 font-bold text-2xl">Detail Pembayaran</div>
-      <div className=" mx-auto bg-white rounded-xl shadow-sm px-6 max-md:text-center max-md:border max-md:text-sm max-md:border-gray-300 max-md:p-2 max-md:w-44 max-md:mx-0">
+    <div className="w-[535px] max-xl:w-full">
+      <div className="pb-4 font-bold text-2xl max-lg:text-xl max-sm:text-lg">
+        Detail Pembayaran
+      </div>
+      <div className=" mx-auto bg-white rounded-xl shadow-sm px-6 max-sm:px-4 ">
         <div className="pt-10">
-          <div className="flex justify-between p-6 text-lg">
+          <div className="flex justify-between p-6 text-lg max-lg:text-base max-sm:text-sm">
             <div className="flex flex-col gap-2">
               <div className="">Harga Tiket</div>
               <div className="">Biaya Administrasi</div>
               <div className="">Pajak 10%</div>
             </div>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 ">
               <div className="font-semibold">0</div>
               <div className="font-semibold">0</div>
               <div className="font-semibold">0</div>
             </div>
           </div>
           <div className="border-b border-gray-300 pb-10 "></div>
-          <div className="flex justify-between pt-10 text-lg">
+          <div className="flex justify-between pt-10 text-lg max-lg:text-base max-sm:text-sm">
             <div className="flex flex-col gap-2">
               <div className="">Harga Total</div>
               <div className="">Tambahan</div>
@@ -59,12 +61,14 @@ export default function DetailPembayaran() {
             </div>
           </div>
           <div className="border-b border-gray-300 pb-10 "></div>
-          <div className="flex justify-between py-10 text-lg">
-            <div className="flex flex-col gap-2">
+          <div className="flex justify-between items-center py-10 text-lg max-lg:text-base max-sm:text-sm">
+            <div className="">
               <div className="">Jumlah Yang Dibayarkan</div>
             </div>
-            <div className="flex flex-col gap-2">
-              <div className="font-bold text-3xl text-[#2A91E5]">0</div>
+            <div className="">
+              <div className="font-bold text-3xl text-[#2A91E5] max-lg:text-2xl max-sm:text-xl">
+                0
+              </div>
             </div>
           </div>
         </div>
--- src/pages/BookingDetail.jsx
+++ src/pages/BookingDetail.jsx
@@ -63,129 +63,165 @@ export default function BookingDetail() {
       <div className="mt-24">
         <CariTiketLain />
       </div>
-      <div className="flex pt-32 gap-8 max-md:flex-col max-md:mx-2 max-md:gap-3  max-lg:pt-40  max-xl:pt-40">
+      <div className="flex pt-32 gap-8max-md:mx-2 max-md:gap-3  max-lg:pt-40  max-xl:pt-40 max-xl:flex-col max-xl:mx-2 ">
         <div className="flex flex-col gap-7">
           <div className="w-full">
-            <div className="pb-4 font-bold text-2xl">
+            <div className="pb-4 font-bold text-2xl max-lg:text-xl max-sm:text-lg">
               Detail Pemesanan Tiket
             </div>
-            <div className=" mx-auto bg-white rounded-xl shadow-sm px-6 max-md:text-center max-md:border max-md:text-sm max-md:border-gray-300 max-md:p-2 max-md:w-44 max-md:mx-0">
-              <div className="pt-10">
+            <div className=" mx-auto bg-gray-200 rounded-xl shadow-sm px-6 max-sm:px-4 ">
+              <div className="py-10 flex flex-col gap-10">
                 <div>
                   <div className="flex items-center gap-4">
                     <GiAirplaneDeparture size={20} />
-                    <div className="font-bold text-base">
+                    <div className="font-bold text-base max-sm:text-sm">
                       Pesawat Keberangkatan
                     </div>
                   </div>
-                  <div className="flex items-center gap-6 pt-4">
+                  <div className="flex items-center gap-6 pt-4 max-lg:gap-4 ">
                     <div>
-                      <div className="font-bold text-[22px]">
+                      <div className="font-bold text-[22px] max-lg:text-lg max-sm:text-sm">
                         {formattedDate}
                       </div>
-                      <div className="font-semibold text-base">Ekonomi</div>
-                    </div>
-                    <div className="flex items-center justify-center ">
-                      <div className="flex flex-col">
-                        <div className="font-bold text-base">Jakarta </div>
-                        <div className=" text-base">(CGK)</div>
-                      </div>
-                      <div className="flex items-center ">
-                        <div className="border-dashed	border-b-2 border-gray-400 w-[60px] mx-2"></div>
-                        <div className="">
-                          <SlPlane className="tilted-icon" size={22} />
-                        </div>
-                        <div className="border-solid border-b-2 border-gray-400 w-[60px] mx-2"></div>
-                      </div>
-                      <div className="flex flex-col">
-                        <div className="font-bold text-base">Medan</div>
-                        <div className="flex justify-between w-full">
-                          <div className="text-base"></div>{" "}
-                          <div className="text-base">(KNO)</div>
-                        </div>
+                      <div className="font-semibold text-base max-lg:text-sm max-sm:text-xs">
+                        Ekonomi
                       </div>
                     </div>
-                    <div className="flex gap-4">
-                      <img
-                        src={AirAsiaLogo}
-                        alt="AirAsia Logo"
-                        className="h-7 w-auto rounded"
-                      />
+                    <div className="flex gap-4 max-sm:flex-col max-sm:gap-2">
+                      <div className="flex items-center justify-center ">
+                        <div className="flex flex-col">
+                          <div className="font-bold text-base max-lg:text-sm ">
+                            Jakarta{" "}
+                          </div>
+                          <div className=" text-base max-lg:text-sm">(CGK)</div>
+                        </div>
+                        <div className="flex items-center ">
+                          <div className="border-dashed	border-b-2 border-gray-400 w-[60px] mx-2 max-lg:w-[30px] max-sm:w-[20px]"></div>
+                          <div className="">
+                            <SlPlane
+                              className="tilted-icon max-lg:size-[18px] max-sm:size-[20px]"
+                              size={22}
+                            />
+                          </div>
+                          <div className="border-solid border-b-2 border-gray-400 w-[60px] mx-2 max-lg:w-[30px] max-sm:w-[20px]"></div>
+                        </div>
+                        <div className="flex flex-col">
+                          <div className="font-bold text-base max-lg:text-sm">
+                            Medan
+                          </div>
+                          <div className="flex justify-between w-full">
+                            <div className="text-base"></div>{" "}
+                            <div className="text-base max-lg:text-sm">
+                              (KNO)
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex gap-4 items-center">
+                        <img
+                          src={AirAsiaLogo}
+                          alt="AirAsia Logo"
+                          className="h-7 w-auto rounded"
+                        />
 
-                      <div className="font-medium text-base">AirAsia</div>
-                    </div>
-                    <div className="flex items-center justify-center ">
-                      <div className="flex flex-col font-semibold text-lg">
-                        19:00
+                        <div className="font-medium text-base max-lg:text-sm">
+                          AirAsia
+                        </div>
                       </div>
-                      <div className="flex items-center">
-                        <div className="border-dashed	border-b-2 border-gray-400 w-[40px] mx-2"></div>
-                        <div className="font-bold text-base">4j 0m</div>
-                        <div className="border-solid border-b border-black w-[40px] mx-2"></div>
-                      </div>
-                      <div className="flex flex-col">
-                        <div className="flex flex-col font-semibold text-lg">
-                          23:00
+
+                      <div className="flex items-center justify-center max-sm:justify-start ">
+                        <div className="flex flex-col font-semibold text-lg max-lg:text-sm ">
+                          19:00
+                        </div>
+                        <div className="flex items-center">
+                          <div className="border-dashed	border-b-2 border-gray-400 w-[40px] mx-2 max-lg:w-[20px] "></div>
+                          <div className="font-bold text-base max-lg:text-sm ">
+                            4j 0m
+                          </div>
+                          <div className="border-solid border-b border-black w-[40px] mx-2 max-lg:w-[20px] "></div>
+                        </div>
+                        <div className="flex flex-col">
+                          <div className="flex flex-col font-semibold text-lg max-lg:text-sm ">
+                            23:00
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="py-10">
+                <div>
                   <div className="flex items-center gap-4">
-                    <GiAirplaneArrival size={20} />
-                    <div className="font-bold text-base">
+                    <GiAirplaneDeparture size={20} />
+                    <div className="font-bold text-base max-sm:text-sm">
                       Pesawat Keberangkatan
                     </div>
                   </div>
-                  <div className="flex items-center gap-6 pt-4">
+                  <div className="flex items-center gap-6 pt-4 max-lg:gap-4 ">
                     <div>
-                      <div className="font-bold text-[22px]">
+                      <div className="font-bold text-[22px] max-lg:text-lg max-sm:text-sm">
                         {formattedDate}
                       </div>
-                      <div className="font-semibold text-base">Ekonomi</div>
-                    </div>
-                    <div className="flex items-center justify-center ">
-                      <div className="flex flex-col">
-                        <div className="font-bold text-base">Medan </div>
-                        <div className=" text-base">(KNO)</div>
-                      </div>
-                      <div className="flex items-center ">
-                        <div className="border-dashed	border-b-2 border-gray-400 w-[60px] mx-2"></div>
-                        <div className="">
-                          <SlPlane className="tilted-icon" size={22} />
-                        </div>
-                        <div className="border-solid border-b-2 border-gray-400 w-[60px] mx-2"></div>
-                      </div>
-                      <div className="flex flex-col">
-                        <div className="font-bold text-base">Jakarta</div>
-                        <div className="flex justify-between w-full">
-                          <div className="text-base"></div>{" "}
-                          <div className="text-base">(CGK)</div>
-                        </div>
+                      <div className="font-semibold text-base max-lg:text-sm max-sm:text-xs">
+                        Ekonomi
                       </div>
                     </div>
-                    <div className="flex gap-4">
-                      <img
-                        src={AirAsiaLogo}
-                        alt="AirAsia Logo"
-                        className="h-7 w-auto rounded"
-                      />
+                    <div className="flex gap-4 max-sm:flex-col max-sm:gap-2">
+                      <div className="flex items-center justify-center ">
+                        <div className="flex flex-col">
+                          <div className="font-bold text-base max-lg:text-sm ">
+                            Jakarta{" "}
+                          </div>
+                          <div className=" text-base max-lg:text-sm">(CGK)</div>
+                        </div>
+                        <div className="flex items-center ">
+                          <div className="border-dashed	border-b-2 border-gray-400 w-[60px] mx-2 max-lg:w-[30px] max-sm:w-[20px]"></div>
+                          <div className="">
+                            <SlPlane
+                              className="tilted-icon max-lg:size-[18px] max-sm:size-[20px]"
+                              size={22}
+                            />
+                          </div>
+                          <div className="border-solid border-b-2 border-gray-400 w-[60px] mx-2 max-lg:w-[30px] max-sm:w-[20px]"></div>
+                        </div>
+                        <div className="flex flex-col">
+                          <div className="font-bold text-base max-lg:text-sm">
+                            Medan
+                          </div>
+                          <div className="flex justify-between w-full">
+                            <div className="text-base"></div>{" "}
+                            <div className="text-base max-lg:text-sm">
+                              (KNO)
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex gap-4 items-center">
+                        <img
+                          src={AirAsiaLogo}
+                          alt="AirAsia Logo"
+                          className="h-7 w-auto rounded"
+                        />
 
-                      <div className="font-medium text-base">AirAsia</div>
-                    </div>
-                    <div className="flex items-center justify-center ">
-                      <div className="flex flex-col font-semibold text-lg">
-                        19:00
+                        <div className="font-medium text-base max-lg:text-sm">
+                          AirAsia
+                        </div>
                       </div>
-                      <div className="flex items-center">
-                        <div className="border-dashed	border-b-2 border-gray-400 w-[40px] mx-2"></div>
-                        <div className="font-bold text-base">4j 0m</div>
-                        <div className="border-solid border-b border-black w-[40px] mx-2"></div>
-                      </div>
-                      <div className="flex flex-col">
-                        <div className="flex flex-col font-semibold text-lg">
-                          23:00
+
+                      <div className="flex items-center justify-center max-sm:justify-start ">
+                        <div className="flex flex-col font-semibold text-lg max-lg:text-sm ">
+                          19:00
+                        </div>
+                        <div className="flex items-center">
+                          <div className="border-dashed	border-b-2 border-gray-400 w-[40px] mx-2 max-lg:w-[20px] "></div>
+                          <div className="font-bold text-base max-lg:text-sm ">
+                            4j 0m
+                          </div>
+                          <div className="border-solid border-b border-black w-[40px] mx-2 max-lg:w-[20px] "></div>
+                        </div>
+                        <div className="flex flex-col">
+                          <div className="flex flex-col font-semibold text-lg max-lg:text-sm ">
+                            23:00
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -195,17 +231,21 @@ export default function BookingDetail() {
             </div>
           </div>
           <div className="w-full">
-            <div className="pb-4 font-bold text-2xl">Detail Penumpang</div>
-            <div className=" mx-auto bg-white rounded-xl shadow-sm px-6 max-md:text-center max-md:border max-md:text-sm max-md:border-gray-300 max-md:p-2 max-md:w-44 max-md:mx-0">
-              <div className="w-3/4 pt-10">
-                <div className="font-semibold text-lg">Informasi Penumpang</div>
-                <div className="flex items-center gap-4 pt-4">
+            <div className="pb-4 font-bold text-2xl max-lg:text-xl max-sm:text-lg">
+              Detail Penumpang
+            </div>
+            <div className=" mx-auto bg-white rounded-xl shadow-sm px-6 max-sm:px-4 ">
+              <div className="w-3/4 pt-10 max-lg:w-full ">
+                <div className="font-semibold text-lg max-md:text-base">
+                  Informasi Penumpang
+                </div>
+                <div className="flex items-center gap-4 pt-4 max-sm:flex-col">
                   <div
                     onClick={handleDropdownToggle}
                     className="flex flex-col gap-2 w-full "
                   >
                     <div className=" text-xs">Jenis Kelamin</div>
-                    <div className="flex  justify-between items-center gap-2 px-4 rounded border-2  border-gray-300  h-14 w-auto ">
+                    <div className="flex  justify-between items-center gap-2 px-4 rounded border-2  border-gray-300  h-14 w-auto max-md:h-12">
                       <div className="font-medium text-sm  ">
                         {jenisKelamin}
                       </div>
@@ -252,7 +292,7 @@ export default function BookingDetail() {
                       required
                       value={namaDepan}
                       onChange={(e) => dispatch(setNamaDepan(e.target.value))}
-                      className="flex  font-medium text-sm items-center  px-2 rounded border-2 border-gray-300 focus:border-sky-500 focus:outline-none h-12 w-full "
+                      className="flex  font-medium text-sm items-center  px-2 rounded border-2 border-gray-300 focus:border-sky-500 focus:outline-none h-12 w-full max-md:h-10 "
                       placeholder="Nama Depan"
                     />
                   </div>
@@ -265,13 +305,13 @@ export default function BookingDetail() {
                       onChange={(e) =>
                         dispatch(setNamaBelakang(e.target.value))
                       }
-                      className="flex  font-medium text-sm items-center  px-2 rounded border-2 border-gray-300 focus:border-sky-500 focus:outline-none h-12 w-full "
+                      className="flex  font-medium text-sm items-center  px-2 rounded border-2 border-gray-300 focus:border-sky-500 focus:outline-none h-12 w-full max-md:h-10 "
                       placeholder="Nama Belakang"
                     />
                   </div>
                   <div className="flex flex-col gap-2 w-full">
                     <div className=" text-xs">Tanggal Lahir</div>
-                    <div className="flex  justify-between items-center gap-2 px-2 rounded border-2 border-gray-300 focus:border-sk h-14 w-auto ">
+                    <div className="flex  justify-between items-center gap-2 px-2 rounded border-2 border-gray-300 focus:border-sk h-14 w-auto max-md:h-12 ">
                       <div>
                         <DatePicker
                           selected={tanggalLahir}
@@ -294,8 +334,10 @@ export default function BookingDetail() {
                   </div>
                 </div>
               </div>
-              <div className="w-3/4 pt-6 pb-10">
-                <div className="font-semibold text-lg">Detail Kontak</div>
+              <div className="w-3/4 pt-6 pb-10 max-lg:w-full">
+                <div className="font-semibold text-lg max-md:text-base">
+                  Detail Kontak
+                </div>
                 <div className="flex items-center gap-4 pt-4">
                   <div className="flex flex-col gap-2 w-full">
                     <div className=" text-xs">Email</div>
@@ -304,7 +346,7 @@ export default function BookingDetail() {
                       required
                       value={email}
                       onChange={(e) => dispatch(setEmail(e.target.value))}
-                      className="flex justify-between text-sm items-center gap-2 px-4 rounded border-2 border-gray-300 focus:border-sky-500 focus:outline-none h-12 w-full pr-10"
+                      className="flex justify-between text-sm items-center gap-2 px-4 rounded border-2 border-gray-300 focus:border-sky-500 focus:outline-none h-12 w-full pr-10 max-md:h-10"
                       placeholder="abc@gmail.com"
                     />{" "}
                   </div>
@@ -315,7 +357,7 @@ export default function BookingDetail() {
                       required
                       value={nomorHP}
                       onChange={(e) => dispatch(setNomorHP(e.target.value))}
-                      className="flex justify-between text-sm items-center gap-2 px-4 rounded border-2 border-gray-300 focus:border-sky-500 focus:outline-none h-12 w-full pr-10"
+                      className="flex justify-between text-sm items-center gap-2 px-4 rounded border-2 border-gray-300 focus:border-sky-500 focus:outline-none h-12 w-full pr-10 max-md:h-10"
                       placeholder="+62 8123456789"
                     />{" "}
                   </div>
